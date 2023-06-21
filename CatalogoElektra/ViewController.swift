c34c00fae6e3040d3ded189dc4e85ae120a7ef67
rinorobot <sgomez_moya@hotmail.com>
2023-06-21 12:23:00 -0600

    Initial Commit

---
+++ CatalogoElektra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatalogoElektra
+//
+//  Created by Salvador Gómez Moya on 21/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
